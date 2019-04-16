fcf5ba49f917734e7c02be9a13f5f0744d5c60dc
Teodora Szasz <dorha.szasz@gmail.com>
2019-04-15 19:40:29 -0500

    Removing padding

--- interact_gui.py
+++ interact_gui.py
@@ -14,13 +14,32 @@ class PointDataAquisition(object):
     User clicks in a slice and the poition of the click will be retrieved.
     """
 
-    def __init__(self, image, window_level=None, figure_size=(10, 8)):
+    def __init__(self, image, figure_size, window_level=None):
         self.image = image
         self.npa, self.min_intensity, self.max_intensity = self.get_window_level_numpy_array(self.image, window_level)
         self.point_indexes = []
 
+        #figure_size = (self.npa.shape[1], self.npa.shape[2])
         # Create a figure.
         self.fig, self.axes = plt.subplots(1, 1, figsize=figure_size)
+        self.axes.axis('off')
+
+        self.axes.spines['top'].set_visible(False)
+        self.axes.spines['right'].set_visible(False)
+        self.axes.spines['bottom'].set_visible(False)
+        self.axes.spines['left'].set_visible(False)
+
+        self.axes.yaxis.set_ticks_position('left')
+        self.axes.xaxis.set_ticks_position('bottom')
+
+        self.axes.xaxis.set_major_locator(plt.NullLocator())
+        self.axes.yaxis.set_major_locator(plt.NullLocator())
+
+        self.axes.get_xaxis().set_ticks([])
+        self.axes.get_yaxis().set_ticks([])
+
+        #self.axes.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        #self.axes.margins(0, 0)
         # Connect the mouse button press to the canvas (__call__ method is the invoked callback).
         self.fig.canvas.mpl_connect('button_press_event', self)
 
@@ -125,10 +144,12 @@ class PointDataAquisition(object):
                 #self.axes.text(text_in_data_coords[0], text_in_data_coords[1], str(i), color='yellow')
         # self.axes.set_title('You selected {0} points'.format(len(self.point_indexes)))
         self.axes.set_axis_off()
+        self.axes.axis('off')
 
         # Set the zoom factor back to what it was before we cleared the axes, and rendered our data.
         self.axes.set_xlim(xlim)
         self.axes.set_ylim(ylim)
+
 
         self.fig.canvas.draw_idle()
 
@@ -169,8 +190,28 @@ class PointDataAquisition(object):
         return [tuple(map(lambda x: int(round(x)), pnt)) for pnt in self.point_indexes]
 
     def save_current_image(self, filename):
+        self.axes.axis('off')
+        self.axes.spines['top'].set_visible(False)
+        self.axes.spines['right'].set_visible(False)
+        self.axes.spines['bottom'].set_visible(False)
+        self.axes.spines['left'].set_visible(False)
+
+        self.axes.yaxis.set_ticks_position('left')
+        self.axes.xaxis.set_ticks_position('bottom')
+
+        self.axes.xaxis.set_major_locator(plt.NullLocator())
+        self.axes.yaxis.set_major_locator(plt.NullLocator())
+
+        self.axes.get_xaxis().set_ticks([])
+        self.axes.get_yaxis().set_ticks([])
+
+        #self.axes.subplots_adjust(top=1, bottom=0, right=1, left=0, hspace=0, wspace=0)
+        #self.axes.margins(0, 0)
+
         ext_filename = filename + ".jpg"
-        self.fig.savefig(ext_filename)
+
+        extent = self.axes.get_window_extent().transformed(self.fig.dpi_scale_trans.inverted())
+        self.fig.savefig(ext_filename, pad_inches=0, bbox_inches=extent, nbins=0, transparent=True)
         img = sitk.ReadImage(ext_filename)
         new_filename = filename + ".dcm"
         sitk.WriteImage(img, new_filename)
